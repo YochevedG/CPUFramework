875aa3be44ffce75a8515d88b59c0bb00b3a2350
yochevedg <yochevedgoldberg1@gmail.com>
2024-05-06 18:29:28 -0400

    initial commit

---
+++ SQLUtility.cs
@@ -0,0 +1,24 @@
+﻿using System.Data;
+using System.Data.SqlClient;
+
+namespace CPUFramework
+{
+    public class SQLUtility
+    {
+        public static string ConnectionString = "";
+        public static DataTable GetDataTable(string sqlstatement)
+        {
+            DataTable dt = new();
+            SqlConnection conn = new();
+            conn.ConnectionString = ConnectionString;
+            conn.Open();
+            //DisplayMessage("Connection Status", conn.State.ToString());
+            var cmd = new SqlCommand();
+            cmd.Connection = conn;
+            cmd.CommandText = sqlstatement;
+            var dr = cmd.ExecuteReader();
+            dt.Load(dr);
+            return dt;
+        }
+    }
+}
