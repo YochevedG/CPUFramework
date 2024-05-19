c1807e275151537879de0c7bfcf1b151198be35e
yochevedg <yochevedgoldberg1@gmail.com>
2024-05-19 16:36:53 -0400

    session 16 work

--- SQLUtility.cs
+++ SQLUtility.cs
@@ -30,6 +30,22 @@ namespace CPUFramework
             GetDataTable(sqlstatement);
         }
 
+        public static int GetFirstRowValue(string sql)
+        {
+            int n = 0;
+            DataTable dt = GetDataTable(sql);
+            if (dt.Rows.Count > 0 && dt.Columns.Count > 0)
+            {
+                if (dt.Rows[0][0] != DBNull.Value) 
+                {
+                    int.TryParse(dt.Rows[0][0].ToString(), out n);
+                }
+                
+            }
+
+            return n;
+        }
+
         private static void SetColumnsAllowNull(DataTable dt)
         {
             foreach(DataColumn c in dt.Columns)
